e709792112363cc9ae6a9674db66774685513565
shreyas <shreyas.dhaware2301@gmail.com>
2022-07-15 23:47:35 -0700

    bert sentiment analysis

---
+++ src/config.py
@@ -0,0 +1,14 @@
+import transformers
+
+DEVICE = "gpu"
+MAX_LEN = 64
+TRAIN_BATCH_SIZE = 8
+VALID_BATCH_SIZE = 4
+EPOCHS = 1
+BERT_PATH = "bert-base-uncased"
+MODEL_PATH = "root/data/model.bin"
+TRAINING_FILE = "root/data/train.csv"
+TOKENIZER = transformers.BertTokenizer.from_pretrained(
+    BERT_PATH, 
+    do_lower_case=True
+    )
